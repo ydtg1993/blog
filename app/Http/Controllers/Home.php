e6e71b7b857221567624dd143b8dfcdf5edef781
946818508@qq.com <scl199303>
2018-09-20 13:12:54 +0800

    stream

---
+++ app/Http/Controllers/Home.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2018/9/20 0020
+ * Time: 上午 10:26
+ */
+
+namespace App\Http\Controllers;
+
+
+use Illuminate\Http\Request;
+use Stream\Stream;
+
+class Home extends Controller
+{
+    public function index(Request $request)
+    {
+        Stream::start();
+        $a = 100000;
+
+        for ($i = 2; $i<$a;$i++)
+        {
+            $c = $i*2;
+        }
+
+        Stream::end();
+    }
+}
