4e18b48dcd73add6097afb13d3c91b723f65a335
946818508@qq.com <scl199303>
2018-09-30 17:47:12 +0800

    *

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -34,15 +34,58 @@
                                         <li><p>性别 : 男</p></li>
                                     </ol>
                                 </div>
-                                <div class="author_chunk chunk">
+                                <div class="release_content_chunk chunk">
+                                    <div class="release_content">
+                                        fdsfdsfdsfdsfdsfdsfd
+                                    </div>
+                                    <div class="release_content_bottom">
 
+                                    </div>
                                 </div>
+
+
                                 <div class="clear_both"></div>
                             </li>
                         </ul>
                     </li>
                 </ul>
 
+                <ul class="forum_table relay_comment">
+                    <li>
+                        <ul class="forum_data">
+                            <li>
+                                <div class="author_chunk chunk">
+                                    <ol>
+                                        <li><p class="p_large">会员</p></li>
+                                        <li>
+                                            <span class="level_tag"></span>
+                                        </li>
+                                        <li><a href=""><img class="avatar_x_large"
+                                                            src="{{URL::asset('img/S.png')}}"></a></li>
+                                        <li><a href="" class="strong_name">hikki</a></li>
+                                        <li>
+                                            <a href="" title="关注">
+                                                <i class="fa fa-gratipay"></i><strong> 121</strong>
+                                            </a>
+                                        </li>
+                                        <li><p>性别 : 男</p></li>
+                                    </ol>
+                                </div>
+                                <div class="release_content_chunk chunk">
+                                    <div class="release_content">
+                                        fdsfdsfdsfdsfdsfdsfd
+                                    </div>
+                                    <div class="release_content_bottom">
+
+                                    </div>
+                                </div>
+
+
+                                <div class="clear_both"></div>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
             </section>
         </div>
 
